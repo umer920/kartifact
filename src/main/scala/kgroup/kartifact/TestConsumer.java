4b795635c43ab6a520a91b0a9fd166c55f5caa77
umer920 <ufbhatti92@gmail.com>
2014-09-04 17:34:22 +0500

    multi thread

--- src/main/scala/kgroup/kartifact/TestConsumer.java
+++ src/main/scala/kgroup/kartifact/TestConsumer.java
@@ -1,220 +1,76 @@
 package kgroup.kartifact;
 
-import kafka.api.FetchRequest;
-import kafka.api.FetchRequestBuilder;
-import kafka.api.PartitionOffsetRequestInfo;
-import kafka.common.ErrorMapping;
-import kafka.common.TopicAndPartition;
-import kafka.javaapi.*;
-import kafka.javaapi.consumer.SimpleConsumer;
-import kafka.message.MessageAndOffset;
- 
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
-import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
- 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class TestConsumer {
-    public static void main(String args[]) {		
-		Properties props = new Properties();
-        props.put("zookeeper.connect", "192.168.23.104:2181");
-        props.put("group.id", "1");
-        props.put("zookeeper.session.timeout.ms", "100000");
-        props.put("zookeeper.sync.time.ms", "203");
-        props.put("auto.commit.interval.ms", "1000");
-        // props.put("auto.offset.reset", "smallest");
-		
-        ConsumerConfig cf = new ConsumerConfig(props) ;
-        
-        ConsumerConnector consumer = Consumer.createJavaConsumerConnector(cf) ;
-        
-        String topic = "multi2" ;
-        
+	private final ConsumerConnector consumer;
+    private final String topic;
+    private  ExecutorService executor;
+ 
+    public TestConsumer(String a_zookeeper, String a_groupId, String a_topic) {
+        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
+                createConsumerConfig(a_zookeeper, a_groupId));
+        this.topic = a_topic;
+    }
+ 
+    public void shutdown() {
+        if (consumer != null) consumer.shutdown();
+        if (executor != null) executor.shutdown();
+    }
+ 
+    public void run(int a_numThreads) {
         Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-        topicCountMap.put(topic, new Integer(1));
+        topicCountMap.put(topic, new Integer(a_numThreads));
         Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
         List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
-	
-	
-        KafkaStream<byte[],byte[]> stream = streams.get(0) ;
-        
-        ConsumerIterator<byte[], byte[]> it = stream.iterator();
-        int i = 1 ;
-        while (it.hasNext()) {
-            System.out.println(i + ": " + new String(it.next().message()));
-            ++i;
-        }
-        
-        consumer.shutdown();
-    }
  
-    private List<String> m_replicaBrokers = new ArrayList<String>();
- 
-    public TestConsumer() {
-        m_replicaBrokers = new ArrayList<String>();
-    }
- 
-    public void run(long a_maxReads, String a_topic, int a_partition, List<String> a_seedBrokers, int a_port) throws Exception {
-        // find the meta data about the topic and partition we are interested in
+        // now launch all the threads
         //
-        PartitionMetadata metadata = findLeader(a_seedBrokers, a_port, a_topic, a_partition);
-        if (metadata == null) {
-            System.out.println("Can't find metadata for Topic and Partition. Exiting");
-            return;
+        executor = Executors.newFixedThreadPool(a_numThreads);
+ 
+        // now create an object to consume the messages
+        //
+        int threadNumber = 0;
+        for (final KafkaStream stream : streams) {
+            executor.submit(new ConsumerTest(stream, threadNumber));
+            threadNumber++;
         }
-        if (metadata.leader() == null) {
-            System.out.println("Can't find Leader for Topic and Partition. Exiting");
-            return;
-        }
-        String leadBroker = metadata.leader().host();
-        String clientName = "Client_" + a_topic + "_" + a_partition;
- 
-        SimpleConsumer consumer = new SimpleConsumer(leadBroker, a_port, 100000, 64 * 1024, clientName);
-        long readOffset = getLastOffset(consumer,a_topic, a_partition, kafka.api.OffsetRequest.EarliestTime(), clientName);
- 
-        int numErrors = 0;
-        while (a_maxReads > 0) {
-            if (consumer == null) {
-                consumer = new SimpleConsumer(leadBroker, a_port, 100000, 64 * 1024, clientName);
-            }
-            FetchRequest req = new FetchRequestBuilder()
-                    .clientId(clientName)
-                    .addFetch(a_topic, a_partition, readOffset, 100000) // Note: this fetchSize of 100000 might need to be increased if large batches are written to Kafka
-                    .build();
-            FetchResponse fetchResponse = consumer.fetch(req);
- 
-            if (fetchResponse.hasError()) {
-                numErrors++;
-                // Something went wrong!
-                short code = fetchResponse.errorCode(a_topic, a_partition);
-                System.out.println("Error fetching data from the Broker:" + leadBroker + " Reason: " + code);
-                if (numErrors > 5) break;
-                if (code == ErrorMapping.OffsetOutOfRangeCode())  {
-                    // We asked for an invalid offset. For simple case ask for the last element to reset
-                    readOffset = getLastOffset(consumer,a_topic, a_partition, kafka.api.OffsetRequest.LatestTime(), clientName);
-                    continue;
-                }
-                consumer.close();
-                consumer = null;
-                leadBroker = findNewLeader(leadBroker, a_topic, a_partition, a_port);
-                continue;
-            }
-            numErrors = 0;
- 
-            long numRead = 0;
-            for (MessageAndOffset messageAndOffset : fetchResponse.messageSet(a_topic, a_partition)) {
-                long currentOffset = messageAndOffset.offset();
-                if (currentOffset < readOffset) {
-                    System.out.println("Found an old offset: " + currentOffset + " Expecting: " + readOffset);
-                    continue;
-                }
-                readOffset = messageAndOffset.nextOffset();
-                ByteBuffer payload = messageAndOffset.message().payload();
- 
-                byte[] bytes = new byte[payload.limit()];
-                payload.get(bytes);
-                System.out.println(String.valueOf(messageAndOffset.offset()) + ": " + new String(bytes, "UTF-8"));
-                numRead++;
-                a_maxReads--;
-            }
- 
-            if (numRead == 0) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ie) {
-                }
-            }
-        }
-        if (consumer != null) consumer.close();
     }
  
-    public static long getLastOffset(SimpleConsumer consumer, String topic, int partition,
-                                     long whichTime, String clientName) {
-        TopicAndPartition topicAndPartition = new TopicAndPartition(topic, partition);
-        Map<TopicAndPartition, PartitionOffsetRequestInfo> requestInfo = new HashMap<TopicAndPartition, PartitionOffsetRequestInfo>();
-        requestInfo.put(topicAndPartition, new PartitionOffsetRequestInfo(whichTime, 1));
-        kafka.javaapi.OffsetRequest request = new kafka.javaapi.OffsetRequest(
-                requestInfo, kafka.api.OffsetRequest.CurrentVersion(), clientName);
-        OffsetResponse response = consumer.getOffsetsBefore(request);
+    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", a_zookeeper);
+        props.put("group.id", a_groupId);
+        props.put("zookeeper.session.timeout.ms", "400");
+        props.put("zookeeper.sync.time.ms", "200");
+        props.put("auto.commit.interval.ms", "1000");
  
-        if (response.hasError()) {
-            System.out.println("Error fetching data Offset Data the Broker. Reason: " + response.errorCode(topic, partition) );
-            return 0;
-        }
-        long[] offsets = response.offsets(topic, partition);
-        return offsets[0];
+        return new ConsumerConfig(props);
     }
  
-    private String findNewLeader(String a_oldLeader, String a_topic, int a_partition, int a_port) throws Exception {
-        for (int i = 0; i < 3; i++) {
-            boolean goToSleep = false;
-            PartitionMetadata metadata = findLeader(m_replicaBrokers, a_port, a_topic, a_partition);
-            if (metadata == null) {
-                goToSleep = true;
-            } else if (metadata.leader() == null) {
-                goToSleep = true;
-            } else if (a_oldLeader.equalsIgnoreCase(metadata.leader().host()) && i == 0) {
-                // first time through if the leader hasn't changed give ZooKeeper a second to recover
-                // second time, assume the broker did recover before failover, or it was a non-Broker issue
-                //
-                goToSleep = true;
-            } else {
-                return metadata.leader().host();
-            }
-            if (goToSleep) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ie) {
-                }
-            }
-        }
-        System.out.println("Unable to find new leader after Broker failure. Exiting");
-        throw new Exception("Unable to find new leader after Broker failure. Exiting");
-    }
+    public static void main(String[] args) {
+        String zooKeeper = args[0];
+        String groupId = args[1];
+        String topic = args[2];
+        int threads = Integer.parseInt(args[3]);
  
-    private PartitionMetadata findLeader(List<String> a_seedBrokers, int a_port, String a_topic, int a_partition) {
-        PartitionMetadata returnMetaData = null;
-        loop:
-        for (String seed : a_seedBrokers) {
-            SimpleConsumer consumer = null;
-            try {
-                consumer = new SimpleConsumer(seed, a_port, 100000, 64 * 1024, "leaderLookup");
-                List<String> topics = Collections.singletonList(a_topic);
-                TopicMetadataRequest req = new TopicMetadataRequest(topics);
-                kafka.javaapi.TopicMetadataResponse resp = consumer.send(req);
+        TestConsumer example = new TestConsumer(zooKeeper, groupId, topic);
+        example.run(threads);
  
-                List<TopicMetadata> metaData = resp.topicsMetadata();
-                for (TopicMetadata item : metaData) {
-                    for (PartitionMetadata part : item.partitionsMetadata()) {
-                        if (part.partitionId() == a_partition) {
-                            returnMetaData = part;
-                            break loop;
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Error communicating with Broker [" + seed + "] to find Leader for [" + a_topic
-                        + ", " + a_partition + "] Reason: " + e);
-            } finally {
-                if (consumer != null) consumer.close();
-            }
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ie) {
+ 
         }
-        if (returnMetaData != null) {
-            m_replicaBrokers.clear();
-            for (kafka.cluster.Broker replica : returnMetaData.replicas()) {
-                m_replicaBrokers.add(replica.host());
-            }
-        }
-        return returnMetaData;
+        example.shutdown();
     }
 }
--- src/main/scala/kgroup/kartifact/TestProducer.java
+++ src/main/scala/kgroup/kartifact/TestProducer.java
@@ -7,37 +7,27 @@ import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
  
 public class TestProducer {
-    public static void main(String []args) {
-    	// TODO Auto-generated method stub
-		
-    			Properties props = new Properties();
-    			 
-    			props.put("metadata.broker.list", "208.95.234.50:9092,208.95.234.54:9092");
-    			props.put("serializer.class", "kafka.serializer.StringEncoder");
-    			// props.put("partitioner.class", "example.producer.SimplePartitioner");
-    			props.put("request.required.acks", "1");
-    			 
-    			ProducerConfig config = new ProducerConfig(props);
-    			
-    			Producer<String, String> producer = new Producer<String, String>(config);
-    			
-    			String date = "04092014" ;
-    			// String topic = "my-replicated-topic" ;
-    			String topic = "multi2" ;
-    			
-    			
-    			for (int i = 1 ; i <= 1000 ; i++) {
-    				
-    				String msg = date + " This is message " + i ;
-    				System.out.println(msg) ;
-    				
-    				KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, String.valueOf(i), msg);
-    				 
-    				producer.send(data);
-    				
-    				
-    			}
-    			
-    			producer.close();
-    }
+	 public static void main(String[] args) {
+	        long events = Long.parseLong(args[0]);
+	        Random rnd = new Random();
+	 
+	        Properties props = new Properties();
+	        props.put("metadata.broker.list", "208.95.234.50:9092,208.95.234.54:9092 ");
+	        props.put("serializer.class", "kafka.serializer.StringEncoder");
+	        props.put("partitioner.class", "example.producer.SimplePartitioner");
+	        props.put("request.required.acks", "1");
+	 
+	        ProducerConfig config = new ProducerConfig(props);
+	 
+	        Producer<String, String> producer = new Producer<String, String>(config);
+	 
+	        for (long nEvents = 0; nEvents < events; nEvents++) { 
+	               long runtime = new Date().getTime();  
+	               String ip = "192.168.2." + rnd.nextInt(255); 
+	               String msg = runtime + ",www.example.com," + ip; 
+	               KeyedMessage<String, String> data = new KeyedMessage<String, String>("page_visits", ip, msg);
+	               producer.send(data);
+	        }
+	        producer.close();
+	    }
 }
